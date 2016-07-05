6bc4c35872855e180b872cf617390a51fc54817e
Young-Hwan Mun <mcodegeeks@gmail.com>
2016-07-05 16:46:21 -0400

    add some files

---
+++ app/src/main/java/com/mcodegeeks/gifmake/SavedGifsActivity.java
@@ -0,0 +1,13 @@
+package com.mcodegeeks.gifmake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedGifsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_gifs);
+    }
+}
