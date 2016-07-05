dcf8e782caf1052a1f2c97e9bca7dd1d953c5616
Young-Hwan Mun <mcodegeeks@gmail.com>
2016-07-05 16:45:01 -0400

    add some files

--- app/src/main/java/com/mcodegeeks/gifmake/SplashActivity.java
+++ app/src/main/java/com/mcodegeeks/gifmake/SplashActivity.java
@@ -1,9 +1,9 @@
 package com.mcodegeeks.gifmake;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
