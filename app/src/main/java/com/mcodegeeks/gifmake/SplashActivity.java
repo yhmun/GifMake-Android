09a6fd7fd2e49c9d631f86881387cec15d10e221
Young-Hwan Mun <mcodegeeks@gmail.com>
2016-07-05 17:13:17 -0400

    add slash screen feature

--- app/src/main/java/com/mcodegeeks/gifmake/SplashActivity.java
+++ app/src/main/java/com/mcodegeeks/gifmake/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.mcodegeeks.gifmake;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(SplashActivity.this, GifEditorActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 1000);
     }
 }
--- app/src/main/java/com/mcodegeeks/gifmake/WelcomeActivity.java
+++ app/src/main/java/com/mcodegeeks/gifmake/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.mcodegeeks.gifmake;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
